cbc02a4d5db86e41fb5fbea4589336c983cf8219
Danii <128658303+DaniIvanike@users.noreply.github.com>
2024-03-12 08:18:33 -0300

    mcndsn

--- jogo.js
+++ jogo.js
@@ -6,6 +6,11 @@ while(chute == ''){
     chute = prompt ('Escolha um número entre 0 a 10.');
 }
 
+while(chute == null || chute != numeroSecreto){
+    alert('Tenta de novo arrombado :)');
+    chute = prompt ('Escolha um número entre 0 a 10.');
+}
+
 if(numeroSecreto == chute){
       alert('Parabéns!!! Você acertou :)');
       console.log('Parabéns, acertou mizera');
