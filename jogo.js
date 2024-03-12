4e673d46283fe8435a74726fd25620396c005e76
Danii <128658303+DaniIvanike@users.noreply.github.com>
2024-03-12 07:54:14 -0300

    shchb

--- jogo.js
+++ jogo.js
@@ -1,1 +1,11 @@
 let numeroSecreto  = 8;
+
+let chute = prompt ('Escolha um número entre 0 a 10.');
+
+if(numeroSecreto == chute){
+      alert('Parabéns!!! Você acertou :)');
+      console.log('Parabéns, acertou mizera');
+} else {
+    alert('Errou infeliz >:(');
+      console.log('Infelizmente você falhou');
+}
