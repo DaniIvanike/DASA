1a8353d039b68efe9194b3774cd0c53f23533fd5
Danii <128658303+DaniIvanike@users.noreply.github.com>
2024-04-02 08:41:06 -0300

    fd

--- cont.js
+++ cont.js
@@ -3,7 +3,7 @@ console.log(botoes.length)
 
 for(let sas = 0; sas < botoes.length; sas++){
     console.log(sas);
-    botoes[sas].classList.add('ativo')
+    botoes[sas].classList.add('ativo');
 
 }
 
