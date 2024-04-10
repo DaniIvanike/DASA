4e5cb6b3d373c7277ae5fe44d5fcfb1eb88c580e
Danii <128658303+DaniIvanike@users.noreply.github.com>
2024-04-10 10:36:59 -0300

    f

--- cont.js
+++ cont.js
@@ -5,7 +5,9 @@ const textos = document.querySelectorAll('.aba-conteudo');
 for (let sas = 0; sas < botoes.length; sas++) {
 
     botoes[sas].onclick = function () {
+        console.log(sas);
         for (let j = 0; j < botoes.length; j++) {
+            console.log(j);
             botoes[j].classList.remove('ativo');
             textos[j].classList.remove('ativa');
         }
