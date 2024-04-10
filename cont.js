5c0878fbf423013d764d8d6a8b502729f40a05ed
Danii <128658303+DaniIvanike@users.noreply.github.com>
2024-04-10 10:36:30 -0300

    g

--- cont.js
+++ cont.js
@@ -1,16 +1,16 @@
 const botoes = document.querySelectorAll('.objetivos');
 const textos = document.querySelectorAll('.aba-conteudo');
-console.log(botoes.length);
+
 
 for (let sas = 0; sas < botoes.length; sas++) {
-    console.log(sas);
+
     botoes[sas].onclick = function () {
         for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove('ativo');
             textos[j].classList.remove('ativa');
         }
         botoes[sas].classList.add('ativo');
-        // textos[sas].classList.add('ativa');
+        textos[sas].classList.add('ativa');
     }
 }
 
