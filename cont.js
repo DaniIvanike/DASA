9caa7d4a3004705c62d34e89aec0dab636e88a0a
Danii <128658303+DaniIvanike@users.noreply.github.com>
2024-04-17 09:36:32 -0300

    cxz

--- cont.js
+++ cont.js
@@ -19,7 +19,10 @@ for (let sas = 0; sas < botoes.length; sas++) {
 const contadores = document.querySelectorAll('.contador');
 const tempoObjetivo1 = new Date ('2024-04-19T00:00:00');
 let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo1 - tempoAtual;
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+let segundos = tempoFinal/1000
+
+contadores[0].textContent = segundos;
 
 
