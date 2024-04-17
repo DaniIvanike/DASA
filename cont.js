2dd74638345a23991cb10c5ee3b173bf96398fef
Danii <128658303+DaniIvanike@users.noreply.github.com>
2024-04-17 10:08:59 -0300

    x

--- cont.js
+++ cont.js
@@ -22,7 +22,8 @@ let tempoAtual = new Date();
 let tempoFinal = tempoObjetivo1 - tempoAtual;
 
 let segundos = tempoFinal/1000
+let minutos = segundos/60
 
-contadores[0].textContent = segundos;
+contadores[0].textContent = minutos;
 
 
